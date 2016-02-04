ab69c484562506581f9d2449b022792c587f4ff6
Timothy <eclisesolaris23@gmail.com>
2016-02-04 14:11:32 -0500

    Initial Commit

---
+++ Cascading Sample/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cascading Sample
+//
+//  Created by newuser on 2/4/16.
+//  Copyright © 2016 Cascading. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
